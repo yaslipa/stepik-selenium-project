d59070b94c26a880da5af4947b538a28bc01da42
Elena Chiriliuc <slipa.box@gmail.com>
2021-07-22 02:43:03 +0300

    Add parametrization of link for a test function

--- test_product_page.py
+++ test_product_page.py
@@ -1,15 +1,23 @@
+import pytest
+
 from pages.product_page import ProductPage
 
+CATALOG_URL = 'http://selenium1py.pythonanywhere.com/catalogue'
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+
+@pytest.mark.parametrize('offer_number',
+                         [*range(0, 7),
+                          pytest.param(7, marks=pytest.mark.xfail),
+                          8, 9])
+def test_guest_can_add_product_to_basket(browser, offer_number):
+    link = f'{CATALOG_URL}/coders-at-work_207/?promo=offer{offer_number}'
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
 
 
 def test_guest_should_see_add_to_basket_btn(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+    link = f'{CATALOG_URL}/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_basket_btn()
